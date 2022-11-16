fa501b1579f958405724581b3b2a3e472303ccc1
adarachel <108772486+adarachel@users.noreply.github.com>
2022-11-16 18:58:53 +0100

    add & delete test
    Co-Authored-By: Christian Kiyong <kiyongbohbo@icloud.com>

--- add-remove.test.js
+++ add-remove.test.js
@@ -1,34 +1,46 @@
-import Todos from "./src/modules/TodoList.js";
-import Todo from "./src/modules/Todo.js";
+/**
+ * @jest-environment jsdom
+ */
 
-describe('Tests', () => {
-    document.body.innerHTML = `<div class="container">
-    <div class="header border-bottom flex">
-        <h1>TODAY'S TO DO </h1>
-        <i class="bx bx-refresh"></i>
-    </div>
-    <form class="flex border-bottom" id="form">
-        <input class="input" type="text" name="todo" placeholder="Add to your list..." />
-        <i class='bx bx-subdirectory-left'></i>
-    </form>
-    <div class="todo-container"></div>
-    <div class="clear">
-        <button id="remove" type="button">Clear All Completed</button>
-    </div>
-</div>`;
-
-const task = 'Do the laundry';
-  const todo = new Todo(task);
-
-  test('Add new item to the list', () => {
-    todo.addTodo();
-    const data = JSON.parse(localStorage.getItem('todos'));
-    expect(data.length).toBe(1);
-  });
-
-  test('Remove new item from the list', () => {
-    Todo.removeTodo(0);
-    const data = JSON.parse(localStorage.getItem('todos'));
-    expect(data.length).toBe(0);
-  });
-});
+ import Todo from './src/modules/Todo.js';
+ import TodoArray from './__mocks__/todomock.js';
+ 
+ const todoArray = new TodoArray();
+ 
+ for (let i = 0; i < 3; i += 1) {
+   const todo = new Todo(`Task ${i}`, false, i + 1);
+   todoArray.addTodo(todo);
+ }
+ 
+ const renderTodos = () => {
+   document.body.innerHTML = '<div id="todo-list"></div>';
+   todoArray.getAllTodos().forEach((todo) => {
+     const todoElement = document.createElement('div');
+     todoElement.classList.add('todo');
+     todoElement.innerHTML = `<input type="text" value="${todo}" />`;
+     document.querySelector('#todo-list').appendChild(todoElement);
+   });
+ };
+ 
+ beforeAll(() => {
+   renderTodos();
+ });
+ 
+ describe('Manipulate todos', () => {
+   test('remove todo', () => {
+     todoArray.deleteTodo(1);
+     renderTodos();
+     expect(todoArray.getAllTodos().length).toBe(2);
+   });
+   test('remove div deleted from the dom', () => {
+     const list = document.querySelector('#todo-list').childNodes;
+     expect(list.length).toBe(2);
+   });
+   test('add todo item', () => {
+     todoArray.addTodo(
+       new Todo('Task list', false, todoArray.getAllTodos().length + 1),
+     );
+     renderTodos();
+     expect(todoArray.getAllTodos().length).toBe(3);
+   });
+ });
