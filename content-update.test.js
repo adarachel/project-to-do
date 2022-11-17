359ffec5fefc4ffa3f1950d534deaf2cfabcfc4e
adarachel <108772486+adarachel@users.noreply.github.com>
2022-11-17 18:10:10 +0100

    fix linter errors

--- content-update.test.js
+++ content-update.test.js
@@ -46,20 +46,20 @@ describe('Manipulate update', () => {
     todoArray.updateTodo(2, newTask2);
     renderTodos();
     expect(todoArray.getAllTodos()[1].description).toBe('Its okay');
-});
+  });
 
   test('check completed status', () => {
     todoArray.changeCompleted(1);
     renderTodos();
     expect(todoArray.getAllTodos()[0].completed).toBe(true);
-});
+  });
 
   test('check dom element is checked completed in checkbox', () => {
     const checkbox = document.querySelectorAll('input[type="checkbox"]');
     expect(checkbox[0].checked).toBe(true);
-});
+  });
 
-test('Clear all completed tasks', () => {
+  test('Clear all completed tasks', () => {
     todoArray.clearAllTodos();
     renderTodos();
     expect(todoArray.getAllTodos().length).toBe(2);
